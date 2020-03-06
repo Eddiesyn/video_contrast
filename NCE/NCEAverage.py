fix slicing error in Average for supervised contrastive

NCE/NCEAverage.py
@@ -325,7 +325,7 @@ class Average(nn.Module):
             pos_temp = indices_temp[i // self.sample_num].copy()
             pos_temp.remove(i)
             neg_temp = indices_temp.copy()
-            neg_temp.pop(i % self.class_num)
+            neg_temp.pop(i // self.sample_num)
 
             self.pos_indices[i, :] = torch.tensor(pos_temp).cuda()  # (sample_num-1, )
             # ((class_num-1)*sample_num, )

NCE/NCECriterion.py
@@ -40,7 +40,7 @@ class KLCriterion(nn.Module):
     def forward(self, x, pos_indices):
         bsz = x.size(0)
         assert bsz == pos_indices.size(0), 'FATAL ERROR!'
-        batchSize = bsz // self.clips_num
+        # batchSize = bsz // self.clips_num
 
         x_other = x[pos_indices]  # (bsz, clips_num-1, x.size(1))
         x_other = torch.mean(x_other, dim=1)  # same shape as x